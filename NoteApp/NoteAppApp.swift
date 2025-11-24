85620009b216a095be6a16ab1b165622b4c9d65a
ahmed-abdelmoula <ahmed.abdelmola@hotmail.com>
2025-11-24 18:02:57 +0100

    stating note app

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Ahmed Mac on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
